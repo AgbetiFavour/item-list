f4b1a9bcd4c59fc0eb49515bee5bf54b54363607
Agbeti Favour <agbetifavour1@gmail.com>
2021-03-22 09:25:46 +0100

    javaScript dom assignment

--- js/dom.js
+++ js/dom.js
@@ -1,163 +1,32 @@
-// EXAMINE THE DOCUMENT OBJECT //
-
-// console.dir(document);
-// console.log(document.domain);
-// console.log(document.URL);
-// console.log(document.title);
-// //document.title =  123;
-// console.log(document.doctype);
-// console.log(document.head);
-// console.log(document.body);
-// console.log(document.all);
-// console.log(document.all[10]);
-// document.all[10].textContent = 'Hello';
-// console.log(document.forms[0]);
-// console.log(document.links);
-// console.log(document.images);
-
-// GETELEMENTBYID //
-// console.log(document.getElementById('header-title'));
-// const headerTitle = document.getElementById('header-title');
-// const header = document.getElementById('main-header');
-// console.log(headerTitle);
-// headerTitle.textContent = 'Hello';
-// headerTitle.innerText = 'Goodbye';
-// console.log(headerTitle.innerText);
-// headerTitle.innerHTML = '<h3>Hello World</h3>';
-// header.style.borderBottom = 'solid 3px #000';
-
-// GETELEMENTSBYCLASSNAME //
-// const items = document.getElementsByClassName('list-group-item');
-// console.log(items);
-// console.log(items[1]);
-// items[1].textContent = 'Hello 2';
-// items[1].style.fontWeight = 'bold';
-// items[1].style.backgroundColor = 'yellow';
-
-// // Gives error
-// //items.style.backgroundColor = '#f4f4f4';
-
-// for(var i = 0; i < items.length; i++){
-//   items[i].style.backgroundColor = '#f4f4f4';
-// }
-
-// GETELEMENTSBYTAGNAME //
-// const li = document.getElementsByTagName('li');
-// console.log(li);
-// console.log(li[1]);
-// li[1].textContent = 'Hello 2';
-// li[1].style.fontWeight = 'bold';
-// li[1].style.backgroundColor = 'yellow';
-
-// // Gives error
-// //items.style.backgroundColor = '#f4f4f4';
-
-// for(const i = 0; i < li.length; i++){
-//   li[i].style.backgroundColor = '#f4f4f4';
-// }
-
-// QUERYSELECTOR //
-// const header = document.querySelector('#main-header');
-// header.style.borderBottom = 'solid 4px #ccc';
-
-// const input = document.querySelector('input');
-// input.value = 'Hello World'
-
-// const submit = document.querySelector('input[type="submit"]');
-// submit.value="SEND"
-
-// const item = document.querySelector('.list-group-item');
-// item.style.color = 'red';
-
-// const lastItem = document.querySelector('.list-group-item:last-child');
-// lastItem.style.color = 'blue';
-
-// const secondItem = document.querySelector('.list-group-item:nth-child(2)');
-// secondItem.style.color = 'coral';
-
-// QUERYSELECTORALL //
-// const titles = document.querySelectorAll('.title');
-
-// console.log(titles);
-// titles[0].textContent = 'Hello';
-
-// const odd = document.querySelectorAll('li:nth-child(odd)');
-// const even= document.querySelectorAll('li:nth-child(even)');
-
-// for(const i = 0; i < odd.length; i++){
-//   odd[i].style.backgroundColor = '#f4f4f4';
-//   even[i].style.backgroundColor = '#ccc';
-// }
+const form = document.getElementById('addForm');
+const itemList = document.getElementById('items');
 
 
-// TRAVERSING THE DOM //
-const itemList = document.querySelector('#items');
-// parentNode
-// console.log(itemList.parentNode);
-// itemList.parentNode.style.backgroundColor = '#f4f4f4';
-// console.log(itemList.parentNode.parentNode.parentNode);
+form.addEventListener('submit', addItem);
+itemList.addEventListener('click', removeItem);
 
-// parentElement
-// console.log(itemList.parentElement);
-// itemList.parentElement.style.backgroundColor = '#f4f4f4';
-// console.log(itemList.parentElement.parentElement.parentElement);
+function addItem(e){
+    e.preventDefault();
 
-// childNodes
-// console.log(itemList.childNodes);
+    const newItem = document.getElementById('item').value;
 
-// console.log(itemList.children);
-// console.log(itemList.children[1]);
-// itemList.children[1].style.backgroundColor = 'yellow';
+    const li = document.createElement('li');
+    li.className = 'list-group-item';
+    li.appendChild(document.createTextNode(newItem));
 
-// // FirstChild
-// console.log(itemList.firstChild);
-// // firstElementChild
-// console.log(itemList.firstElementChild);
-// itemList.firstElementChild.textContent = 'Hello 1';
+    const deleteBtn = document.createElement('button');
+    deleteBtn.className = 'btn btn-danger btn-sm float-right delete';
+    deleteBtn.appendChild(document.createTextNode('x'));
+    li.appendChild(deleteBtn)
 
+    itemList.appendChild(li);
+}
 
-// lastChild
-// console.log(itemList.lastChild);
-// lastElementChild
-// console.log(itemList.lastElementChild);
-// itemList.lastElementChild.textContent = 'Hello 4';
-
-// nextSibling
-// console.log(itemList.nextSibling);
-// // nextElementSibling
-// console.log(itemList.nextElementSibling);
-
-// previousSibling
-// console.log(itemList.previousSibling);
-// previousElementSibling
-// console.log(itemList.previousElementSibling);
-// itemList.previousElementSibling.style.color = 'green';
-
-// createElement
-
-// Create a div
-const newDiv =  document.createElement('div');
-
-// Add class
-newDiv.className= 'hello';
-
-// Add id
-newDiv.id = 'hello1';
-
-// Add attr
-newDiv.setAttribute('title', 'Hello Div');
-
-// Create text node
-const newDivText = document.createTextNode('Hello World');
-
-// Add text to div
-newDiv.appendChild(newDivText);
-
-const container = document.querySelector('header .container');
-const h1 = document.querySelector('header h1');
-
-console.log(newDiv);
-
-newDiv.style.fontSize = '30px';
-
-container.insertBefore(newDiv, h1);
+function removeItem(e) {
+    if (e.target.classList.contains('delete')) {
+        if (confirm('Are You Sure?')) {
+            const li = e.target.parentElement;
+            itemList.removeChild(li);
+        }
+    }
+}
